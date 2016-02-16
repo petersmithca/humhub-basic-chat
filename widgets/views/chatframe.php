5057413148036dea3afe6ab64ab7c595e99b8747
WebCrew <palareas@gmail.com>
2016-02-16 02:15:52 +0100

    Update chatframe.php
    I had to update it to make use of bootstrap buttons and input-groups. Now the submit form looks better and we dont need a button image :)

--- widgets/views/chatframe.php
+++ widgets/views/chatframe.php
@@ -4,22 +4,33 @@ use yii\helpers\Html;
 use humhub\models\Setting;
 ?>
 <div class="panel">
-    <div class="panel-heading"><?php echo '<strong>Humhub</strong> Chat'; ?></div>
-    <div class="panel-body">
-        <div id="chatContainer">
-
-            <div id="chatLineHolder"></div>
-
-            <div id="chatUsers" class="rounded"></div>
-            <div id="chatBottomBar" class="rounded">
-                <div class="tip"></div>
-                <form id="submitForm" method="post" action="">
-                    <input id="chatText" name="chatText" class="rounded" maxlength="255" />
-                    <input type="submit" class="blueButton" value="Submit" />
-                </form>
-
-            </div>
-
+  <div class="panel-heading">
+    <?php echo '<strong>Community</strong> Chat'; ?>
+  </div>
+  <div class="panel-body">
+    <div id="chatContainer">
+      
+      <div id="chatLineHolder">
+      </div>
+      
+      <div id="chatUsers" class="rounded">
+      </div>
+      <div id="chatBottomBar" class="rounded">
+        <div class="tip">
         </div>
+        <form id="submitForm" action="#" method="post" _lpchecked="1">
+          <div class="input-group">
+            <input id="chatText" type="text" name="chatText" placeholder="Type Message ..." class="form-control" maxlength="510">
+            <span class="input-group-btn">
+              <button type="submit" value="Submit" class="btn btn-primary btn-flat">
+                Send
+              </button>
+            </span>
+          </div>
+        </form>
+        
+      </div>
+      
     </div>
+  </div>
 </div>
