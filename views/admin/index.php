bd48aa2c3c8c359cb59e30f3d44d5dde64205b6b
Tobciu <code@tobj.de>
2016-02-19 17:19:27 +0100

    translation

--- views/admin/index.php
+++ views/admin/index.php
@@ -17,7 +17,7 @@ use humhub\modules\humhubchat\controllers\AdminController;
 				<?=$form->labelEx($model, 'timeout'); ?>
 				<?=$form->textField($model, 'timeout', ['class' => 'form-control', 'readonly' => Setting::IsFixed('timeout', 'humhubchat')]); ?>
 			</div>
-			<p class="help-block"><?=Yii::t('Humhub-chatModule.base', 'days, after messages will be deleted; min value is 0 (no deletion); max: 365'); ?></p>
+			<p class="help-block"><?=Yii::t('Humhub-chatModule.base', 'amount of days, after messages will be deleted; min value is 0 (no deletion); max: 365'); ?></p>
 			<?= CHtml::submitButton(Yii::t('Humhub-chatModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
 			<?=\humhub\widgets\DataSaved::widget(); ?>
 		<?php CActiveForm::end(); ?>
