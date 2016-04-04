7dd8de568a485526e9c07fb6bc67547beb6eb5b4
Sarah Tsumayoi <Felli@users.noreply.github.com>
2016-04-03 21:05:24 -0400

    Fixed: Replaced CHtml with Html
    Needs tested before merged with the master branch. Still need to look over replacing the CActiveForm and the coding needed to be done.

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,24 +1,25 @@
 <?php
+use yii\helper\URL
+use yii\helpers\Html
 use humhub\compat\CActiveForm;
-use humhub\compat\CHtml;
 use humhub\models\Setting;
 use humhub\modules\humhubchat\controllers\AdminController;
 ?>
 <div class="panel panel-default">
-	<div class="panel-heading"><?=Yii::t('Humhub-chatModule.base', '<strong>Humhub-Chat</strong>'); ?></div>
+	<div class="panel-heading"><?=Yii::t('Humhub-chatModule.views_humhubchat_index', '<strong>Basic Chat</strong>'); ?></div>
 	<div class="panel-body">
 		<?php $form = CActiveForm::begin(['id' => 'hhc-settings-form']); ?>
 			<?=$form->errorSummary($model); ?>
 			<div class="form-group">
 				<?=$form->labelEx($model, 'theme'); ?>
 				<?=$form->dropDownList($model, 'theme', AdminController::getThemes(), ['class' => 'form-control', 'readonly' => Setting::IsFixed('theme', 'humhubchat')]); ?>
-			</div>
+				</div>
 			<div class="form-group">
 				<?=$form->labelEx($model, 'timeout'); ?>
 				<?=$form->textField($model, 'timeout', ['class' => 'form-control', 'readonly' => Setting::IsFixed('timeout', 'humhubchat')]); ?>
 			</div>
-			<p class="help-block"><?=Yii::t('Humhub-chatModule.base', 'amount of days, after messages will be deleted; min value is 0 (no deletion); max: 365'); ?></p>
-			<?= CHtml::submitButton(Yii::t('Humhub-chatModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
+			<p class="help-block"><?=Yii::t('Humhub-chatModule.views_humhubchat_index', 'Amount of days, after messages will be deleted; min value is 0 (no deletion); max: 365'); ?></p>
+			<?php echo Html::submitButton(Yii::t('Humhub-chatModule.views_humhubchat_index', 'Save'), ['class' => 'btn btn-primary']); ?>
 			<?=\humhub\widgets\DataSaved::widget(); ?>
 		<?php CActiveForm::end(); ?>
 	</div>
