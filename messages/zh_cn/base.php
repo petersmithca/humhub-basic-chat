1dc6fd2395d29d79ffce527876c69b3af839da5a
Sarah Tsumayoi <Felli@users.noreply.github.com>
2016-04-17 18:14:44 -0400

    Enh: Adds Chinese language support
    Might need some changing done later as I'm not 100% about my Chinese translations.

---
+++ messages/zh_cn/base.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'Basic Chat' => '基本聊天',
+    '<strong>Basic Chat</strong>' => '<strong>基本聊天</strong>',
+    'ID' => 'ID',
+    'author' => '作者',
+    'created at' => '在創建',
+    'message' => '消息',
+    '{count} person online' => '{count} 線上的人',
+    '{count} people online' => '{count} 線上的人',
+    'theme' => '主題',
+    'timeout' => '超時',
+    'save' => '保存',
+    'dark theme' => '黑暗的主題',
+    'light theme' => '光的主題',
+    'amount of days, after messages will be deleted; min value is 0 (no deletion); max: 365' => '天之後將刪除消息; 量最小值是 0 （沒有刪除）;最大 ︰ 365',
+    '<strong>Community</strong> Chat' => '<strong>基本</strong> 聊天',
+    'Type Message ...' => '鍵入消息...',
+    'send' => '發送'
+];
